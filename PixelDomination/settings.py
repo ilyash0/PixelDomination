3b4c4ab4717b0924f6d2c30cc048804916eee9e3
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2024-12-14 15:22:29 +0500

    Update user module

--- PixelDomination/settings.py
+++ PixelDomination/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 AUTH_USER_MODEL = 'user.CustomUser'
-LOGIN_URL = '/login/'
+LOGIN_URL = '/'
 
 # Application definition
 
--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth import authenticate, login, logout
@@ -7,48 +8,46 @@ from user.models import CustomUser
 
 
 class RegisterView(View):
-    template_name = 'register.html'
-
-    def get(self, request):
-        return render(request, self.template_name)
-
     def post(self, request):
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        confirm_password = request.POST.get('confirm_password')
+        email = request.POST.get('reg-email')
+        username = request.POST.get('reg-username')
+        password = request.POST.get('reg-password')
+        confirm_password = request.POST.get('reg-confirm-password')
 
         if password != confirm_password:
-            return render(request, self.template_name, {'error': 'Passwords do not match'})
+            messages.error(request, 'Пароли не совпадают')
+            return redirect('index')
 
         if CustomUser.objects.filter(username=username).exists():
-            return render(request, self.template_name, {'error': 'Username already exists'})
+            messages.error(request, 'Пользователя с таким именем уже существует')
+            return redirect('index')
 
-        CustomUser.objects.create_user(username=username, password=password)
-        return redirect('login')
+        if CustomUser.objects.filter(email=email).exists():
+            messages.error(request, 'Пользователь с такой почтой уже существует')
+            return redirect('index')
+
+        CustomUser.objects.create_user(username=username, password=password, email=email)
+        return redirect('profile')
 
 
 class LoginView(View):
-    template_name = 'login.html'
-
-    def get(self, request):
-        return render(request, self.template_name)
-
     def post(self, request):
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.POST.get('login-username')
+        password = request.POST.get('login-password')
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
             return redirect('profile')
         else:
-            return render(request, self.template_name, {'error': 'Invalid credentials'})
+            messages.error(request, 'Неверное имя пользователя или пароль')
+            return redirect('index')
 
 
 class LogoutView(View):
     def get(self, request):
         logout(request)
-        return redirect('login')
+        return redirect('index')
 
 
 class UserProfileView(LoginRequiredMixin, View):
@@ -57,4 +56,3 @@ class UserProfileView(LoginRequiredMixin, View):
     def get(self, request):
         user = request.user
         return render(request, self.template_name, {'user_profile': user})
-
