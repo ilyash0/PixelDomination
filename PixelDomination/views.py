b184e61519cb25bfcff2dc5465c37cc86c20cbea
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2024-12-15 01:45:44 +0500

    Add rating to index page

--- PixelDomination/views.py
+++ PixelDomination/views.py
@@ -1,5 +1,18 @@
-from django.views.generic.base import TemplateView
+from django.db.models import Window, F
+from django.db.models.functions import RowNumber
+from django.shortcuts import render
+from django.views.generic.base import TemplateView, View
+
+from user.models import CustomUser
 
 
-class IndexView(TemplateView):
+class IndexView(View):
     template_name = "index.html"
+
+    def get(self, request):
+        rating = CustomUser.objects.annotate(
+            rank=Window(
+                expression=RowNumber(), order_by=F('pixels_colored').desc()
+            )
+        ).order_by('-pixels_colored')
+        return render(request, self.template_name, {'rating': rating[:5]})
