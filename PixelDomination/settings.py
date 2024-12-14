789094e157864bb8aefc6b08750b476d04f0d66e
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2024-12-15 03:00:44 +0500

    Allowed all hosts

--- PixelDomination/settings.py
+++ PixelDomination/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 AUTH_USER_MODEL = 'user.CustomUser'
 LOGIN_URL = '/login'
 
